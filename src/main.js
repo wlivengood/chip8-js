e6ca408641e9eb49b5b0c27db8f2fbe2a9713ce1
Will Livengood <wlivengo@gmail.com>
2017-03-13 20:23:46 -0400

    changed a var to a let, some functions to arrow functions

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 const {fetchFile, handleUpload} = require('./handlers');
 
 (function () {
-	document.addEventListener('DOMContentLoaded',function() {
+	document.addEventListener('DOMContentLoaded',() => {
 	    document.querySelector('select[name="games"]').onchange=fetchFile;
 	},false);
 
-	document.addEventListener('DOMContentLoaded', function() {
-		var fileInput = document.getElementById("fileInput");
+	document.addEventListener('DOMContentLoaded', () => {
+		let fileInput = document.getElementById("fileInput");
 		fileInput.addEventListener("change", handleUpload, false);
 	}, false);
 })();
